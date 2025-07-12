bf9cd313612baf3435b121da23da877d9bea4fc9
Rishi Vanukuru <rishi.vanukuru@gmail.com>
2025-07-12 00:02:40 -0600

    First full media push

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 // Get references to the elements you'll be working with
 var input = document.getElementById("password");
 var div = document.getElementById("HIDDENDIV");
+var faildiv = document.getElementById("HIDDENFAIL");
 var btn = document.getElementById("button");
 
 // Set up event handlers in JavaScript
 button.addEventListener("click", validate);
 
 function validate(){
-  if (input.value == 'PASSWORD') { 
+  if (input.value == 'CARDAMOM' || input.value == 'cardamom' || input.value == 'Cardamom') { 
      // No need to add a "show" class. Just remove the "hidden" class.
      div.classList.remove('hidden');
-     
+     if(!faildiv.classList.contains('hidden'))
+     {
+      faildiv.classList.add('hidden');
+     }
      // Or, add it:
      // input.classList.add("hidden");
   } else {  
-     password.focus(); // <-- If you don't do this first, your select code won't work
-     password.setSelectionRange(0, password.value.length);   
-     alert('Spell failed!'); 
-     div.classList.add('hidden');
+     //password.focus(); // <-- If you don't do this first, your select code won't work
+     //password.setSelectionRange(0, password.value.length);   
+     //alert('Spell failed! Do not attempt to impersonate Fairwind, or you will be cursed!'); 
+     //div.classList.add('hidden');
+     if(!div.classList.contains('hidden'))
+     {
+      div.classList.add('hidden');
+     }
+     faildiv.classList.remove('hidden');
+
   }
 }
-
-input.addEventListener("keydown", function(event){
- if (event.keyCode === 13){
-   // No reason to simulate a button click. Just call the code that needs to be run.
-   validate();
- }
-});
